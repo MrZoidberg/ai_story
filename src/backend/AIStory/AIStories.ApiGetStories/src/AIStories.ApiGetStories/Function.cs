89ba001845a84b2fef755e67a5abd145113a6063
Mike Merkulov <mihail.merkulov@gmail.com>
2023-04-12 21:49:52 +0300

    fixed cors for api

--- src/backend/AIStory/AIStories.ApiGetStories/src/AIStories.ApiGetStories/Function.cs
+++ src/backend/AIStory/AIStories.ApiGetStories/src/AIStories.ApiGetStories/Function.cs
@@ -127,7 +127,7 @@ public class Function
                 {
                     { "Content-Type", "application/json" },
                     { "Access-Control-Allow-Headers", "Content-Type"},
-                    { "Access-Control-Allow-Origi", "*"},
+                    { "Access-Control-Allow-Origin", "*"},
                     { "Access-Control-Allow-Methods", "OPTIONS,GET" }
                 }
             };
